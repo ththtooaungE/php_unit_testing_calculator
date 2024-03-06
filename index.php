e3c688c47252118aee532d0c433d92a8c88f71fd
Thet Htoo Aung <ththtooaung.e@gmail.com>
2024-03-06 16:59:00 +0630

    Code update

--- index.php
+++ index.php
@@ -1,3 +1,41 @@
+<?php
+include 'config.php';
+include 'Calculator.php';
+
+if ($_POST) {
+    $num1 = $_POST['num1'];
+    $num2 = $_POST['num2'];
+    $operator  = $_POST['operator'];
+    $calculator = new Calculator();
+
+    switch ($operator) {
+        case '+':
+            $result = $calculator->add($num1, $num2);
+            break;
+
+        case '-':
+            $result = $calculator->subtract($num1, $num2);
+            break;
+
+        case '*':
+            $result = $calculator->multiply($num1, $num2);
+            break;
+
+        case '/':
+            $result = $calculator->devide($num1, $num2);
+            break;
+
+        default:
+            $result = "Invalid operator";
+            break;
+    }
+
+    $stmt = $pdo->prepare("INSERT INTO calculations (num1, num2, operator, result) VALUES (?, ?, ?, ?)");
+    $stmt->execute([$num1, $num2, $operator, $result]);
+}
+
+?>
+
 <!DOCTYPE html>
 <html lang="en">
 
@@ -8,19 +46,63 @@
 </head>
 
 <body>
-    <h1>Calculator</h1>
+    <div class="card">
+        <h1>Calculator</h1>
 
-    <form action="result.php" method="POST">
-        <input type="number" name="num1" placeholder="Enter a number">
-        <select name="operator" id="" required>
-            <option value="+">+</option>
-            <option value="-">-</option>
-            <option value="*">*</option>
-            <option value="/">/</option>
-        </select>
-        <input type="number" name="num2" placeholder="Enter a number">
-        <input type="submit" value="Calculate">
-    </form>
+        <form action="index.php" method="POST" class="card2">
+            <input type="number" name="num1" placeholder="Enter a number">
+            <select name="operator" id="" required>
+                <option value="+">+</option>
+                <option value="-">-</option>
+                <option value="*">*</option>
+                <option value="/">/</option>
+            </select>
+            <input type="number" name="num2" placeholder="Enter a number">
+            <input type="submit" value="Calculate">
+        </form>
+
+        <?php if (isset($result)) : ?>
+            <h2>Result: <?php echo $result; ?></h2>
+        <?php endif; ?>
+    </div>
+
+
+    <!-- <style>
+        .card {
+            width: 400px;
+            margin-left: auto;
+            margin-right: auto;
+            /* background-color: gray; */
+
+        }
+
+        .card>h1 {
+            text-align: center;
+        }
+
+        .card2 {
+            margin-left: auto;
+            margin-right: auto;
+            width: 300px;
+            padding: 5px;
+            border-radius: 10px;
+            background-color: gray;
+
+        }
+
+        form>input,
+        select,
+        option {
+            display: block;
+        }
+
+        input,
+        select,
+        option {
+            padding: 15px;
+            border-radius: 10px;
+        }
+    </style> -->
 </body>
 
 </html>
